0699863f518c1800b94b229799c3cbcfd918aa9d
Gabriel Trindade Rocha <gabriel.oficioti@gmail.com>
2024-08-20 15:07:14 -0300

    update clienteController

--- server/src/main/java/com/projetoNicks/server/Controller/ClienteController.java
+++ server/src/main/java/com/projetoNicks/server/Controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.projetoNicks.server.Controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoNicks.server.DTO.ClienteDTO;
 import com.projetoNicks.server.DTO.ClienteDetalhadoDTO;
+import com.projetoNicks.server.Entities.ClienteEntity;
 import com.projetoNicks.server.Repository.ClienteRepository;
 import com.projetoNicks.server.Service.ClienteService;
 
@@ -20,21 +22,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ClienteController {
 
-	private final ClienteRepository clienteRepository;
 	private final ClienteService clienteService;
 
+    ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
 	//Traz todos os clientes na base de dados
-	@GetMapping
-	public List<ClienteDTO> getAllClientes(){
-		return clienteService.getAllClientes();
+	@GetMapping("/allClientes")
+	public List<ClienteEntity> getAllClientes(){
+		return clienteService.getList();
 	}
 	
 	//Traz os clientes com o id selecionado
-	@GetMapping("/{id}")
-	 public ResponseEntity<ClienteDetalhadoDTO> buscarClienteDetalhado(@PathVariable Long id) {
-        ClienteDetalhadoDTO clienteDetalhadoDTO = clienteService.buscarClienteDetalhado(id);
-        return ResponseEntity.ok(clienteDetalhadoDTO);
-    }
+//	@GetMapping("/{id}")
+//	 public ResponseEntity<ClienteDetalhadoDTO> buscarClienteDetalhado(@PathVariable Long id) {
+//        ClienteDetalhadoDTO clienteDetalhadoDTO = clienteService.buscarClienteDetalhado(id);
+//        return ResponseEntity.ok(clienteDetalhadoDTO);
+//    }
 	
 	
 	
