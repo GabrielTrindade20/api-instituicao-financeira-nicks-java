844a632d89f4ef0447a6ebff339e3178f44df990
Gabriel Trindade Rocha <gabriel.oficioti@gmail.com>
2024-08-20 15:07:01 -0300

    update service

--- server/src/main/java/com/projetoNicks/server/Service/ClienteService.java
+++ server/src/main/java/com/projetoNicks/server/Service/ClienteService.java
@@ -23,58 +23,8 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	public List<ClienteDTO> getAllClientes(){
-		return clienteRepository.findAll().stream()
-				.map(cliente -> new ClienteDTO(
-						cliente.getIdCliente(),
-						cliente.getNome(),
-						cliente.getEmail(),
-						cliente.getTelefone(),
-						cliente.getCpf()
-						))
-				.collect(Collectors.toList());
+	public List<ClienteEntity> getList() {
+		return clienteRepository.findAll();
 	}
-	
-	public Optional<ClienteDTO> getClienteById(Long id){
-		return clienteRepository.findById(id)
-				.map(cliente -> new ClienteDTO(
-						cliente.getIdCliente(),
-						cliente.getNome(),
-						cliente.getEmail(),
-						cliente.getTelefone(),
-						cliente.getCpf()
-						));			
-	}
-	
-	 public ClienteDetalhadoDTO buscarClienteDetalhado(Long idCliente) {
-	        ClienteEntity clienteEntity = clienteRepository.findById(idCliente)
-	                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
 
-	        ClienteDetalhadoDTO clienteDetalhadoDTO = new ClienteDetalhadoDTO();
-	        clienteDetalhadoDTO.setIdCliente(clienteEntity.getIdCliente());
-	        clienteDetalhadoDTO.setNome(clienteEntity.getNome());
-	        clienteDetalhadoDTO.setEmail(clienteEntity.getEmail());
-	        clienteDetalhadoDTO.setCpf(clienteEntity.getCpf());
-	        clienteDetalhadoDTO.setTelefone(clienteEntity.getTelefone());
-	        clienteDetalhadoDTO.setSenha(clienteEntity.getSenha());
-
-	        // Preencher dados do endereço
-	        EnderecoEntity endereco = clienteEntity.getEnderecos().get(0); // Supondo que o cliente tenha apenas um endereço
-	        clienteDetalhadoDTO.setIdEndereco(endereco.getId());
-	        clienteDetalhadoDTO.setLogradouro(endereco.getLogradouro());
-	        clienteDetalhadoDTO.setBairro(endereco.getBairro());
-	        clienteDetalhadoDTO.setNumero(endereco.getNumero());
-	        clienteDetalhadoDTO.setCep(endereco.getCep());
-	        clienteDetalhadoDTO.setCidade(endereco.getCidade());
-	        clienteDetalhadoDTO.setEstado(endereco.getEstado());
-
-	        // Preencher dados da conta
-	        ContaEntity conta = clienteEntity.getContas().get(0); // Supondo que o cliente tenha apenas uma conta
-	        clienteDetalhadoDTO.setIdConta(conta.getId());
-	        clienteDetalhadoDTO.setCodigoConta(conta.getCodigo());
-	        clienteDetalhadoDTO.setSaldoConta(conta.getSaldo());
-
-	        return clienteDetalhadoDTO;
-	    }
-	
 }
